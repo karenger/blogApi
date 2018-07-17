109c0579acc99ec5ea3ef1a65b91b817f8a353d7
karenger <39539551+karenger@users.noreply.github.com>
2018-07-17 00:00:49 -0400

    Segundo commit

--- access/js/login-api.js
+++ access/js/login-api.js
@@ -1,14 +1,33 @@
-//patron para crear una instancia, los () al final permiten invocar la funcion.
-// var plantilla1 = "<tr><td><h3><b>${title}</b></h3><p>${body}</p><a id='profile'href='profile.html'>${userId}(${email})</a></td></tr>";
+var plantillaPost = "<div class='col-lg-12 col-md-12' ><aside style='border-radius: 20px;'><div class='content-title'style='border-top-left-radius: 20px; border-top-right-radius: 20px'><h3><a href='comments.html?postId=${id}'>${title}</a></h3><p style='font-size: 25px'>${body}</p></div><div class='content-footer'><img class='user-small-img' src='image/account.png'><a href='profile.html?UserId=${userId}'style='font-size: 20px;color: #570d40b9;font-weight:bold'>${userId}</a></div> </aside></div>"
+var plantillaPostProfile = "<h3 style='display:inline !important'><a href='comments.html?postId=${id}'>${title}...</a></h3><br>"
+var plantillaPostComment = "<div class='col-lg-12 col-md-12' ><aside style='border-radius: 20px;'><div class='content-title'style='border-top-left-radius: 20px; border-top-right-radius: 20px'><h3><a href='comments.html?postId=${id}'>${title}</a></h3><p style='font-size: 25px'>${body}</p></div><div class='content-footer'><img class='user-small-img' src='image/account.png'><a href='profile.html?UserId=${userId}'style='font-size: 20px;color: #570d40b9;font-weight:bold'><p id='user' onload='LoginApi.getUser(${userId})'>${name}</p>${userId}</a></div></aside></div>"
+var plantillaComment = "<div class='col-lg-12 col-md-12'  ><aside style='border-radius: 20px;' ><div class='content-title' style='border-top-left-radius: 20px; border-top-right-radius: 20px'><br><p style='font-size: 25px'>${body}</p></div><div class='content-footer' ><img class='user-small-img' src='image/account.png'><a id= 'profile'href='profile.html?UserId=${userId}'style='font-size: 20px;color: #570d40b9;font-weight:bold'>${userId}</a></div></aside></div>"
+var plantillaMensaje = "<h1 style='font-weight:bold;font-size: 40px;	font-family: Open sans'>${mensaje}</h1>"
+// var plantillaTitulo = "<br><h1 style='font-size:55px; font-weight:bold;text-align: center; font-family: 'Open sans'; color: white;'>${mensaje}</h1><br>"
+var introducirComment = "<style type='text/css' >::placeholder { color: white; font-weight:bold; font-size:20px; }</style><i class='fa fa-comments' aria-hidden='true' style='font-size:40px'></i><textarea name='textarea' style='columns: 1%' class='input'  id='comment' placeholder='Add a comment...'></textarea><br><br><input style='margin-left:23px;background: #570d40b9;font-size: 25px;font-family: 'Open San'' type='button' onclick='LoginApi.newComment()' class='button' id='btnComment' value='Comment'><br><br><br>"
+var plantillaCantidadComment="${contador}"
+var plantillaUser = "<div class='col-lg-9 col-md-9'><aside><div class='content-footer' class='text-center'><img class='user-small-img' src='image/account.png'><label style='font-size: 50px;text-align: center; color: #fff'>${name}</label></div><div class='content-title'> <br><br><label style='font-size: 35px; margin-left:35px'>Email:  </label><label style='font-size: 35px'>&nbsp; ${email}</label><br><br></div><div class='content-footer' class='text-center'><label style='font-size: 40px;text-align: center; color: #fff'><br>&nbsp&nbsp&nbspPosts<br></label></div><div class='content-title'> <p id='Tabla_Post'></p></div></aside></div>"
 
+var PlantillaNombre = "${id}"
 
-var plantillaIndex ="<div class='col-lg-12 col-md-12' ><aside style='border-radius: 20px;'><div class='content-title'style='border-top-left-radius: 20px; border-top-right-radius: 20px'><h3><a href='#'>${title}</a></h3><p style='font-size: 25px'>${body}</p></div><div class='content-footer'><img class='user-small-img' src='image/account.png'><a id= 'profile'href='profile.html'style='font-size: 20px;color: #570d40b9;font-weight:bold'>${userId}</a></div></aside></div>"
-
-var plantillaUser = "<a href='#'>${name}(${email})</a>";
-var plantillaPost = "<h2>${title}</h2><p>${body}</p></td></tr>";
-$.template("indexTemplate", plantillaIndex);
+$.template("postTemplate", plantillaPost);
+$.template("postProfileTemplate", plantillaPostProfile);
+$.template("commentTemplate", plantillaComment);
+// $.template("tituloTemplate", plantillaTitulo);
+$.template("mensajeTemplate", plantillaMensaje);
+$.template("introducirComment", introducirComment);
 $.template("userTemplate", plantillaUser);
+$.template("nameTemplate", PlantillaNombre);
+$.template("CantidadCommentTemplate", plantillaCantidadComment);
 
+function getQueryParam(param) {
+    location.search.substr(1)
+        .split("&")
+        .some(function (item) { // returns first occurence and stops
+            return item.split("=")[0] == param && (param = item.split("=")[1])
+        })
+    return param
+}
 
 var LoginApi = (function () {
     //TODO: Base_Url
@@ -18,7 +37,8 @@ var LoginApi = (function () {
     var PATH_LOGOUT = "/logout";
     var PATH_POST = "/post";
     var PATH_USERS = "/users";
-    var PATH_USERS = "/";
+    var PATH_COMMENTS = "/comment";
+
 
     return {
         login: function (email, password) {
@@ -71,11 +91,9 @@ var LoginApi = (function () {
                 $.ajax({
                     method: 'DELETE',
                     url: baseUrl + PATH_LOGOUT,
-
-                    headers: {'Authorization': 'Bearer ' + token},
+                    headers: { 'Authorization': 'Bearer ' + token },
                     success: function (data) {
                         resolve(data);
-                        console.log("ffff");
                     },
                     error: function (error) {
                         reject(error);
@@ -86,17 +104,156 @@ var LoginApi = (function () {
         },
         getpost: function () {
             return new Promise(function (resolve, reject) {
+               
                 $.ajax({
                     method: 'GET',
                     url: baseUrl + PATH_POST,
-                    headers: {'Authorization': 'Bearer ' + window.localStorage.getItem("token")},
+                    headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") },
                     success: function (data) {
-                        $.tmpl("indexTemplate", data).appendTo("#Tabla_Post");
-                        data.forEach(usuario => {
-                            getuser(usuario.userId);
+                        var contador;
+                        data.forEach(element => { var cont=0;
+                            //Buscar cantidad de comentarios
+                            $.ajax({
+                                method: 'GET',
+                                url: baseUrl + PATH_POST + "/" + element.id + PATH_COMMENTS,
+                                headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") },
+                                success: function (data1) {
+                                    for (var i = 0; i <= data1.length; i++) { cont = i; }
+                                  }
+                            });
+                            //Buscar cnombre
+                            $.ajax({
+                                method: 'GET',
+                                url: baseUrl + PATH_USERS + "/" + element.userId,
+                                headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") },
+                                success: function (data3) {
+                                 
+                                  $.tmpl("<div class='col-lg-12 col-md-12' ><aside style='border-radius: 20px;'><div class='content-title'style='border-top-left-radius: 20px; border-top-right-radius: 20px'><h3><a href='comments.html?postId="+element.id+"'>"+element.title+"</a></h3><p style='font-size: 25px'>"+element.body+"</p></div><div class='content-footer'><img class='user-small-img' src='image/account.png'><a href='profile.html?UserId="+element.userId+"'style='font-size: 20px;color: #570d40b9;font-weight:bold'>"+data3.name+"</a> <label id='cantComment' style='	margin-left:10px!important'><i  style=' color:black !important' class='fa fa-comments fa-2x' aria-hidden='true'></i>&nbsp;&nbsp;"+cont+"</label></div> </aside></div>").appendTo("#Tabla_Post"); 
+                                }
+                            });
+
+                            
+                             });
+                        
+                        console.log(data);
+                    },
+                    error: function (error) {
+                        reject(error);
+                    }
+                });
+            });
+
+        },
+        getUserProfile: function () {
+            return new Promise(function (resolve, reject) {
+                $.ajax({
+                    method: 'GET',
+                    url: baseUrl + PATH_USERS + "/" + getQueryParam('UserId'),
+                    headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") },
+                    success: function (data) {
+                        $.tmpl("userTemplate", data).appendTo("#profil");
+                        //get post
+                        $.ajax({
+                            method: 'GET',
+                            url: baseUrl + PATH_POST,
+                            headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") },
+                            success: function (data) {
+                                var contador = 0;
+                                data.forEach(element => {
+                                    if (element.userId == getQueryParam('UserId')) { contador = contador + 1; console.log(element); }
+                                });
+                                //Presentar comentarios
+                                if (contador == 0) { $.tmpl("mensajeTemplate", mensaje = [{ mensaje: "No post yet." }]).appendTo("#Tabla_Post"); }
+                                if (contador >= 1) {
+                                    data.forEach(element => {
+                                        if (element.userId == getQueryParam('UserId')) { $.tmpl("postProfileTemplate", element).appendTo("#Tabla_Post"); }
+                                    });
+                                }
+                                console.log(data);
+                            }
                         });
                         console.log(data);
 
+                    }
+                });
+            });
+
+        },
+        getcomment: function () {
+            return new Promise(function (resolve, reject) {
+
+                $.ajax({
+                    method: 'GET',
+                    url: baseUrl + PATH_POST + "/" + getQueryParam('postId') + PATH_COMMENTS,
+                    headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") },
+                    success: function (data) {
+                        var contador = 0;
+                        // BUSCAR EL POST
+                        $.ajax({
+                            method: 'GET',
+                            url: baseUrl + PATH_POST + "/" + getQueryParam('postId'),
+                            headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") },
+                            success: function (data) {
+                                $.tmpl("postTemplate", data).appendTo("#Tabla_Post");
+                                $.tmpl("introducirComment", introducirComment).appendTo("#Tabla_Post");
+                                console.log(data);
+                            }
+                        });
+                        for (var i = 0; i < data.length; i++) { contador = i; }
+                        //Presentar comentarios
+                        if (contador == null) {
+                            $.tmpl("mensajeTemplate", mensaje = [{ mensaje: "No comments yet." }]).appendTo("#mensaje");
+                        }
+                        else {
+                            //$.tmpl("tituloTemplate", titulo = [{ mensaje: "Comments" }]).appendTo("#titulo");
+                            $.tmpl("commentTemplate", data).appendTo("#Tabla_Comment");
+                        }
+                        
+
+                    },
+                    error: function (error) {
+                        reject(error);
+                    }
+                });
+            });
+
+        },
+        
+        newComment: function (commentw) {
+            return new Promise(function (resolve, reject) {
+                var commentw = $('#comment').val();
+                var ld = { body: commentw }
+                $.ajax({
+                    method: 'POST',
+                    data: JSON.stringify(ld),
+                    url: baseUrl + PATH_POST + "/" + getQueryParam('postId') + PATH_COMMENTS,
+                    headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") },
+                    success: function (data) {
+                        window.location.reload();
+                        console.log(data);
+                        window.location.reload("index.html");
+                    },
+                    error: function (error) {
+                        reject(error);
+                    }
+                });
+            });
+
+        },
+        newPost: function (title, body) {
+            return new Promise(function (resolve, reject) {
+                var ld = {
+                    title: title,
+                    body: body
+                }
+                $.ajax({
+                    method: 'POST',
+                    data: JSON.stringify(ld),
+                    url: baseUrl + PATH_POST,
+                    headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") },
+                    success: function (data) {
+                        console.log(data);
+                        window.location = "index.html";
                     },
                     error: function (error) {
                         reject(error);
@@ -106,7 +263,10 @@ var LoginApi = (function () {
 
         }
 
+
+
+
     }
 })();
 
-    
+
--- access/js/login.js
+++ access/js/login.js
@@ -4,8 +4,8 @@ function login() {
     var password = $("#password").val();
 
     //Espacios en blanco
-    if (email == "") {alert("Introduzca su correo.");return;}
-    if (password == "") {alert("Introduzca su contrase;a.");return;}
+    if (email == "") {alert("Enter your email.");return;}
+    if (password == "") {alert("Enter your password.");return;}
 
     LoginApi.login(email,password)
     //them si es exitoso
@@ -21,14 +21,22 @@ function login() {
     //execption
     .catch(function (error) {console.log("Error: ", error);});
 }
+
 function signup() {
     var name = $("#name").val();
     var email = $("#email").val();
     var password = $("#password").val();
     var password2 = $("#password2").val();
+
+    var span= document.getElementById('span');;
+
+    if (name == "") {span.innerText = "Enter your full name.";return;}
+    if (email == "") {span.innerText ="Enter your email.";return;}
+    if (password == "") {span.innerText ="Enter your  password.";return;}
+    if (password2 == "") {span.innerText ="Enter the password confirmation.";return;}
     
     if (password != password2) {
-        alert("Las contraseñas son diferentes.");
+        alert("The passwords are different.");
         return;   
     }
 
@@ -46,6 +54,47 @@ function signup() {
     });
     
 }
+function newpost() {
+    var title = $("#title").val();
+    var body = $("#body").val();
+
+    if (title == "") {span.innerText ="Enter the title.";return;}
+    if (body == "") {span.innerText ="Enter the description.";return;}
+    
+
+    LoginApi.newPost(title, body)
+    //them si es exitoso
+    .then(function(response){
+        console.log("Successfully: ", response);
+    })
+    //execption
+    .catch(function (error) {
+        alert("Error:intentelo de nuevamente")
+        console.log("Error: ", error);
+
+    });
+    
+}
+
+function newcomment() {
+    var body = $("#comment").val();
+
+    if (body == "") {span.innerText ="Enter the description.";return;}
+    
+
+    LoginApi.newComment(body)
+    //them si es exitoso
+    .then(function(response){
+        console.log("Successfully: ", response);
+    })
+    //execption
+    .catch(function (error) {
+        alert("Error:intentelo de nuevamente")
+        console.log("Error: ", error);
+
+    });
+    
+}
 
 window.onload=function () {
     //Validar si existe un token mandarlo al index
@@ -53,19 +102,16 @@ window.onload=function () {
     $("#btnLogin").click(function() {
         login();
     });
-    $("#btnSignup").click(function() {
-        signup();
-    });
-
-}
-window.history.forward=function () {
-    //Validar si existe un token mandarlo al index
-    
-    $("#btnLogin").click(function() {
-        login();
+    $("#btnPost").click(function() {
+        newpost();
     });
     $("#btnSignup").click(function() {
         signup();
     });
-
+    $("#btnatras").click(function() {
+        window.location="login.html";
+    });
+    $("#btnComment").click(function() {
+        newcomment();
+    });
 }
